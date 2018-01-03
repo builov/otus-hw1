f36d55f48e53707c834dff955956f8a9ff29e1fd
Dev 5 <builov@inbox.ru>
2018-01-03 18:50:31 +0300

    first commit

---
+++ lib.php
@@ -0,0 +1,19 @@
+<?php
+
+class MyLib
+{
+	public static function check_str($str) {
+		$chars = str_split($str, 1);
+		$c = 0;
+		foreach ($chars as $char) {
+			if ($char == '(') $c++;
+			else if ($char == ')') $c--;
+			else if ($char != "\t" && $char != "\n" && $char != "\r" && $char != ' ')
+				throw new Exception('InvalidArgumentException');
+			//echo $char . ': ' . $c . "\n";
+		}
+		return ($c == 0) ? 'true' : 'false';
+	}
+}
+
+echo MyLib::check_str('(()()()()))((((()()()))(()()()(((( )))))))');
